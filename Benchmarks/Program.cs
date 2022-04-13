9849e154d4a950772d3b3fd97fbb18d2876ad1f9
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-04-13 21:53:35 +0200

    Added benchmark

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Server;
+using System.Drawing;
+
+BenchmarkRunner.Run<Screenshot>();
+
+public class Screenshot
+{
+    ScreenCapture sc = new ScreenCapture();
+    Graphics graphics = null;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        using var img = sc.CaptureScreen();
+        graphics = Graphics.FromImage(img);
+    }
+
+    [Benchmark]
+    public MemoryStream CaptureImage()
+    {
+        using var stream = new MemoryStream();
+        using var img = sc.CaptureScreen();
+        graphics.DrawImage(img, 160, 120);
+        img.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+        return stream;
+    }
+}
