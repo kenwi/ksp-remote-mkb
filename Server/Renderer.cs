117b50de7d46472025a4046021feeee5c54f7fc5
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-04-20 01:57:21 +0200

    Implemented renderer with js-invokable drawImage

---
+++ Server/Renderer.cs
@@ -0,0 +1,74 @@
+﻿using Blazor.Extensions;
+using Blazor.Extensions.Canvas.WebGL;
+using Microsoft.JSInterop;
+using Server.Services;
+using WebGL;
+
+public class Renderer
+{
+    readonly BECanvasComponent canvas;
+    private readonly DirectXScreenshotService directXScreenshotService;
+    private readonly IJSRuntime jSRuntime;
+    WebGLContext? gl;
+    WebGLUniformLocation u_matrix_location;
+    WebGLProgram shader;
+    static Random rng = new Random((int)DateTime.Now.Ticks);
+    private float[] positions;
+    uint positionAttributeLocation;
+    private ShaderProgram shaderProgram;
+    private byte[] byteArr;
+
+    public Renderer(BECanvasComponent canvas, 
+        DirectXScreenshotService directXScreenshotService,
+        IJSRuntime jSRuntime)
+    {
+        this.canvas = canvas;
+        this.directXScreenshotService = directXScreenshotService;
+        this.jSRuntime = jSRuntime;
+    }
+
+    public async Task Draw()
+    {
+        var img = directXScreenshotService.Image64;
+        await jSRuntime.InvokeVoidAsync("LoadTexture", img, canvas.Width, canvas.Height);
+    }
+
+    private async Task<WebGLProgram> InitProgramAsync(WebGLContext gl, string vsSource, string fsSource)
+    {
+        var vertexShader = await this.LoadShaderAsync(gl, ShaderType.VERTEX_SHADER, vsSource);
+        var fragmentShader = await this.LoadShaderAsync(gl, ShaderType.FRAGMENT_SHADER, fsSource);
+
+        var program = await gl.CreateProgramAsync();
+        await gl.AttachShaderAsync(program, vertexShader);
+        await gl.AttachShaderAsync(program, fragmentShader);
+        await gl.LinkProgramAsync(program);
+
+        await gl.DeleteShaderAsync(vertexShader);
+        await gl.DeleteShaderAsync(fragmentShader);
+
+        if (!await gl.GetProgramParameterAsync<bool>(program, ProgramParameter.LINK_STATUS))
+        {
+            string info = await gl.GetProgramInfoLogAsync(program);
+            throw new Exception("An error occured while linking the program: " + info);
+        }
+        //u_matrix_location = await gl.GetUniformLocationAsync(program, "u_matrix");
+        return program;
+    }
+    
+    private async Task<WebGLShader> LoadShaderAsync(WebGLContext gl, ShaderType type, string source)
+    {
+        var shader = await gl.CreateShaderAsync(type);
+
+        await gl.ShaderSourceAsync(shader, source);
+        await gl.CompileShaderAsync(shader);
+
+        if (!await gl.GetShaderParameterAsync<bool>(shader, ShaderParameter.COMPILE_STATUS))
+        {
+            string info = await gl.GetShaderInfoLogAsync(shader);
+            await gl.DeleteShaderAsync(shader);
+            throw new Exception("An error occured while compiling the shader: " + info);
+        }
+
+        return shader;
+    }
+}
--- Server/wwwroot/JavaScript.js
+++ Server/wwwroot/JavaScript.js
@@ -1,10 +1,10 @@
-﻿function LoadTexture(image64) {
+﻿function LoadTexture(image64, width, height) {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     var img = new Image();
     img.src = image64;
     img.onload = function () {
-        ctx.drawImage(img, 0, 0, 800, 600);
+        ctx.drawImage(img, 0, 0, width, height);
     };
     img.onerror = function (ex) {
         console.log("Error: " + ex);
@@ -16,4 +16,20 @@ function RenderImage(image64) {
     var gl = canvas.getContext("webgl");
     gl.clearColor(0.2, 0.2, 0.2, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+function SetupEvents(dotnetRef) {
+    var canvas = document.getElementById("canvas");
+    canvas.addEventListener('mousedown', function (evt) {
+        dotnetRef.invokeMethodAsync('eventMouseDown', evt.offsetX, evt.offsetY);
+        console.log("mousedown");
+    }, false);
+    canvas.addEventListener('mouseup', function (evt) {
+        dotnetRef.invokeMethodAsync('eventMouseUp', evt.offsetX, evt.offsetY);
+        console.log("mouseup");
+    }, false);
+    canvas.addEventListener('mousemove', function (evt) {
+        dotnetRef.invokeMethodAsync('eventMouseMove', evt.offsetX, evt.offsetY);
+        console.log("mousemove");
+    }, false);
 }
