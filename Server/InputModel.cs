b196ce41e20fbd3af7436cbe54e695de381d50ba
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-04-29 14:26:23 +0200

    "Refactoring"

---
+++ Server/InputModel.cs
@@ -0,0 +1,8 @@
+﻿public class InputModel
+{
+    public string? Link { get; set; }
+    public string? Resolution { get; set; }
+    public string? ImageFormat { get; set; }
+    public string? FrameRate { get; set; }
+    public string? Message { get; set; }
+}
